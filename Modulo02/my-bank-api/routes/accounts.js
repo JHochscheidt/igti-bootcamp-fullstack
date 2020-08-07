e59e7765fcc2fa9f38246ecf2f30ed17f3d1e778
Jackson Henrique Hochscheidt <jackson.hochscheidt@gmail.com>
2020-08-07 19:38:05 -0300

    validação de campos

--- Modulo02/my-bank-api/routes/accounts.js
+++ Modulo02/my-bank-api/routes/accounts.js
@@ -7,11 +7,17 @@ const router = express.Router();
 router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
+
     const data = JSON.parse(await readFile(global.fileName));
+
+    if (!account.name || account.balance === null) {
+      throw new Error('Name e Balance são obrigatórios!');
+    }
 
     account = {
       id: data.nextId++,
-      ...account,
+      name: account.name,
+      balance: account.balance,
     };
 
     data.accounts.push(account);
@@ -68,9 +74,18 @@ router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
 
+    if (!account.id || !account.name || account.balance === null) {
+      throw new Error('Id, Name e Balance são obrigatórios!');
+    }
+
     const data = JSON.parse(await readFile(global.fileName));
     const index = data.accounts.findIndex((a) => a.id === +account.id);
-    data.accounts[index] = account;
+
+    if (index === -1) {
+      throw new Error('Registro não encontrado!');
+    }
+    data.accounts[index].name = account.name;
+    data.accounts[index].balance = account.balance;
 
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
@@ -85,8 +100,17 @@ router.patch('/updateBalance', async (req, res, next) => {
   try {
     const account = req.body;
 
+    if (!account.id || account.balance === null) {
+      throw new Error('Id e Balance são obrigatórios!');
+    }
+
     const data = JSON.parse(await readFile(global.fileName));
     const index = data.accounts.findIndex((a) => a.id === +account.id);
+
+    if (index === -1) {
+      throw new Error('Registro não encontrado!');
+    }
+
     data.accounts[index].balance = account.balance;
 
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
