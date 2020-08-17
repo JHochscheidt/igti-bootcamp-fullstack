04a38600c19403dbc3b0c0070c56b70e13f272f7
Jackson Henrique Hochscheidt <jackson.hochscheidt@gmail.com>
2020-08-17 18:15:23 -0300

    finalizado desafio guiado 04

--- Modulo03/aula12-DesafioGuiado04/frontend/src/App.js
+++ Modulo03/aula12-DesafioGuiado04/frontend/src/App.js
@@ -1,43 +1,83 @@
 import React, { useState, useEffect } from 'react';
 
-import * as api from './api/apiService'
+import * as api from './api/apiService';
 import Spinner from './components/Spinner';
 import GradesControl from './components/GradesControl';
-
+import ModalGrade from './components/ModalGrade';
 
 export default function App() {
   const [allGrades, setAllGrades] = useState([]);
   const [selectedGrade, setSelectedGrade] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-
-  useEffect( () => {
-    const getGrades = async() => {
-      const grades = api.getAllGrades().then( grades => {
-        setTimeout( () => {
+  useEffect(() => {
+    const getGrades = async () => {
+      const grades = api.getAllGrades().then((grades) => {
+        setTimeout(() => {
           setAllGrades(grades);
         }, 2000);
       });
     };
-    getGrades();    
+    getGrades();
   }, []);
 
-  const handleDelete = () => {
-    console.log('handleDelete')
-    
-  }
-  const handlePersist = () => {
-    console.log('handlePersist')
-  }
+  const handleDelete = async (gradeToDelete) => {
+    const isDeleted = await api.deleteGrade(gradeToDelete);
+    console.log(isDeleted);
+    if (isDeleted) {
+      const deletedGradeIndex = allGrades.findIndex(
+        (grade) => grade.id === gradeToDelete.id
+      );
+      const newGrades = Object.assign([], allGrades);
+      newGrades[deletedGradeIndex].isDeleted = true;
+      newGrades[deletedGradeIndex].value = 0;
+      setAllGrades(newGrades);
+    }
+  };
+  const handlePersist = (grade) => {
+    setSelectedGrade(grade);
+    setIsModalOpen(true);
+  };
+  const handlePersistData = async (formData) => {
+    const { id, newValue } = formData;
 
+    const newGrades = Object.assign([], allGrades);
+
+    const gradeToPersist = newGrades.find((grade) => grade.id === id);
+
+    gradeToPersist.value = newValue;
+    if (gradeToPersist.isDeleted) {
+      gradeToPersist.isDeleted = false;
+      await api.insertGrade(gradeToPersist);
+    } else {
+      await api.updateGrade(gradeToPersist);
+    }
+
+    setIsModalOpen(false);
+  };
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
-      <h1 className='center'>Controle de Notas</h1>
+      <h1 className="center">Controle de Notas</h1>
 
-      {allGrades.length === 0 && <Spinner/> }
-      {allGrades.length > 0 &&  <GradesControl grades={allGrades} onDelete={handleDelete} onPersist={handlePersist}/> }
+      {allGrades.length === 0 && <Spinner />}
+      {allGrades.length > 0 && (
+        <GradesControl
+          grades={allGrades}
+          onDelete={handleDelete}
+          onPersist={handlePersist}
+        />
+      )}
+      {isModalOpen && (
+        <ModalGrade
+          onSave={handlePersistData}
+          onClose={handleClose}
+          selectedGrade={selectedGrade}
+        />
+      )}
     </div>
   );
-  
 }
--- Modulo03/aula12-DesafioGuiado04/frontend/src/components/GradesControl.js
+++ Modulo03/aula12-DesafioGuiado04/frontend/src/components/GradesControl.js
@@ -1,37 +1,122 @@
-import React from 'react'
+import React from 'react';
+import Action from './Action';
 
-export default function GradesControl({grades, onDelete, onPersist}) {
-    return (
-        <div className="container center">
-            <table className='striped'>
-                <thead>
-                    <tr>
-                        <th>Aluno</th>
-                        <th>Disciplina</th>
-                        <th>Avaliação</th>
-                        <th>Nota</th>
-                        <th>&nbsp;</th>
-                        <th>&nbsp;</th>
+export default function GradesControl({ grades, onDelete, onPersist }) {
+  const tableGrades = [];
+
+  let currentStudent = grades[0].student;
+  let currentSubject = grades[0].subject;
+  let currentGrades = [];
+  let id = 1;
+
+  grades.forEach((grade) => {
+    if (grade.subject !== currentSubject) {
+      tableGrades.push({
+        id: id++,
+        student: currentStudent,
+        subject: currentSubject,
+        grades: currentGrades,
+      });
+      currentSubject = grade.subject;
+      currentStudent = grade.student;
+      currentGrades = [];
+    }
+    currentGrades.push(grade);
+  });
+
+  tableGrades.push({
+    id: id++,
+    student: currentStudent,
+    subject: currentSubject,
+    grades: currentGrades,
+  });
+
+  const handleActionClick = (id, type) => {
+    const grade = grades.find((grade) => grade.id === id);
+    if (type === 'delete') {
+      onDelete(grade);
+      return;
+    }
+    onPersist(grade);
+  };
+  return (
+    <div className="container center">
+      {tableGrades.map(({ id, grades }) => {
+        const finalGrade = grades.reduce((acc, curr) => acc + curr.value, 0);
+        const classGrade =
+          finalGrade >= 70 ? styles.goodGrade : styles.badGrade;
+        return (
+          <table style={styles.table} className="striped" key={id}>
+            <thead>
+              <tr>
+                <th style={{ width: '20%' }}>Aluno</th>
+                <th style={{ width: '20%' }}>Disciplina</th>
+                <th style={{ width: '20%' }}>Avaliação</th>
+                <th style={{ width: '20%' }}>Nota</th>
+                <th style={{ width: '20%' }}>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {grades.map(
+                ({ id, student, subject, type, value, isDeleted }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{student}</td>
+                      <td>{subject}</td>
+                      <td>{type}</td>
+                      <td>{isDeleted ? '-' : value}</td>
+                      <th>
+                        <div>
+                          <Action
+                            onActionClick={handleActionClick}
+                            id={id}
+                            type={isDeleted ? 'add' : 'edit'}
+                          />
+                          {!isDeleted && (
+                            <Action
+                              onActionClick={handleActionClick}
+                              id={id}
+                              type="delete"
+                            />
+                          )}
+                        </div>
+                      </th>
                     </tr>
-                </thead>
-                <tbody>
-                    {grades.map( ({id, student, subject, type, value, isDeleted}) => {
-                        return(
-                            <tr key={id}>
-                                <td>{student}</td>
-                                <td>{subject}</td>
-                                <td>{type}</td>
-                                <td>{value}</td>
-                                <th>&nbsp;</th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-                <tfoot>
-
-                </tfoot>
-            </table>
-        </div>
-    )
+                  );
+                }
+              )}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td style={{ textAlign: 'right' }}>
+                  <strong>Total</strong>
+                </td>
+                <td>
+                  <span style={classGrade}>{finalGrade}</span>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        );
+      })}
+    </div>
+  );
 }
+
+const styles = {
+  goodGrade: {
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  badGrade: {
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  table: {
+    margin: '20px',
+    padding: '10px',
+  },
+};
