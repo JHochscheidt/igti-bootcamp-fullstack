38009470c1e126404e06ac2a7e61d420e6640d2d
Jackson Henrique Hochscheidt <jackson.hochscheidt@gmail.com>
2020-08-19 21:42:30 -0300

    remove aulas

--- Modulo03/react-prontos/prontos/aula-09-functional-components/react-lifecycle-functional/src/components/User/Avatar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import css from './avatar.module.css';
-
-export default function Avatar({ data }) {
-  const { picture, name } = data;
-
-  return <img className={css.image} src={picture} alt={name} />;
-}
