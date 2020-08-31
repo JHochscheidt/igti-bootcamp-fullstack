d78edb12595c3d33eb688f92a7642fe7214c5426
Jackson Hochscheidt <jackson.hochscheidt@gmail.com>
2020-08-31 10:58:24 -0300

    alterado para funcionar com .env

--- Modulo04/trabalhoPratico/app.js
+++ Modulo04/trabalhoPratico/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv'; 
+
+dotenv.config()
 
 import { connectDatabase } from './database/connectDatabase.js';
 import { accountRouter } from './routes/accountRouter.js';
-
+const PORT = process.env.SERVER_PORT;
 
 const database = connectDatabase();
 
@@ -11,6 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(accountRouter);
 
-app.listen(process.env.SERVER_PORT , () => {
+app.listen(PORT , () => {
   console.log('API Started at port : ' + PORT);
 });
--- Modulo04/trabalhoPratico/database/connectDatabase.js
+++ Modulo04/trabalhoPratico/database/connectDatabase.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
-const DBNAME = process.env.DB_NAME;
-const PASSWORD = process.env.DB_PWD;
+
+import dotenv from 'dotenv';
+dotenv.config()
+
+const DBNAME = process.env.DBNAME;
+const PASSWORD = process.env.DBPWD;
 
 const connectDatabase = async () => {
   try {
